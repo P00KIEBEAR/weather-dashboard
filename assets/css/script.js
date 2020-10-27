80e6c309cdfb5191609254280974d555b9fede86
pat paggi <patpaggi@gmail.com>
2020-10-27 00:08:09 -0700

    five day

--- assets/css/script.js
+++ assets/css/script.js
@@ -8,9 +8,10 @@ var temp = document.querySelector('.temp');
 var wind = document.querySelector('.wind');
 var humidity = document.querySelector('.humidity')
 var uv = document.querySelector('uv')
+var icon = document.createElement('img');
 var iconcode = [];
-var icon = []
-var day = [];
+
+var day = '';
 var days = [];
 var process = []
 
@@ -29,54 +30,60 @@ searchBtn.addEventListener('click', function () {
     console.log(data);
     showData()
   }).catch(function (error) {
-    // if there's an error, log it
+
     console.log(error);
   });
 })
-var geticon = function () {
-  fetch("http://openweathermap.org/w/" + iconcode + "img.png")
-    .then(response => response)
-    // request was successful
-    .then(function (data) {
-      icon = data
 
-    }).catch(function (error) {
-      // if there's an error, log it
-      console.log(error);
-    })
-}
+//need to add link to list
+
 var showData = function () {
 
-  iconcode = days[1].weather[0].icon;
+  //need current date
+  var s = new Date(days[1].dt).toLocaleDateString("en-US")
+  console.log(s)
+
+  icon.src = "http://openweathermap.org/img/w/" + days[1].weather[0].icon + ".png";
   place.innerHTML = days[0].city.name;
   temp.innerText = days[1].main.temp;
   humidity.innerText = days[1].main.humidity;
   wind.innerText = days[1].wind.speed;
+  place.appendChild(icon);
 
-  // need for loop
+
+
+
+  var dateArr = [days[0].list[6].dt_txt, days[0].list[12].dt_txt, days[0].list[18].dt_txt, days[0].list[24].dt_txt, days[0].list[30].dt_txt];
+  var iconArr = [days[0].list[6].weather[0].icon, days[0].list[12].weather[0].icon, days[0].list[18].weather[0].icon, days[0].list[24].weather[0].icon, days[0].list[30].weather[0].icon];
+  var tempArr = [days[0].list[6].main.temp, days[0].list[12].main.temp, days[0].list[18].main.temp, days[0].list[24].main.temp, days[0].list[30].main.temp];
+  console.log(dateArr)
+
+  // need to search and check time to make sure it allways noon may have to use moment?
+
+  for (let i = 0; i < dateArr.length; i++) {
+
+    var dateSlpit = dateArr[i].split(" ");
+    //still need to reverse date
+    var card = document.createElement('div');
+    card.setAttribute('class', 'col-2 mx-auto');
+    card5.appendChild(card);
+    var dateEl = document.createElement('p')
+    dateEl.innerHTML = dateSlpit[0];
+    card.appendChild(dateEl);
+    var tempEl = document.createElement('a')
+    tempEl.innerHTML = tempArr[i] + '°F';
+    card.appendChild(tempEl);
+    console.log(iconArr[i])
+    var iconImg = document.createElement('img')
+    iconImg.src = "http://openweathermap.org/img/w/" + iconArr[i] + ".png";
+    dateEl.appendChild(iconImg)
+
+
+  }
 }
-// five day forcast
-/*
-searchBtn.addEventListener('click', function () {
 
-      var dateArr = [days["list"][6]["dt_txt"], days["list"][12]["dt_txt"], days["list"][18]["dt_txt"], days["list"][24]["dt_txt"], days["list"][30]["dt_txt"]];
-              for (let i = 0; i < dateArr.length; i++) {
-        var card = document.createElement('div').className = 'col-2';
-        var dateEl = document.createElement('p')
-        var tempEl = document.createElement('p')
-        var humidityEL = document.createElement('p')
 
-        card5.appendChild(card);
-        card.appendChild(dateEl)
-        dateEl.innerHTML = dateArr[i];
 
-*/
-/*
 
-var  = "http://openweathermap.org/img/w/" + iconcode + "@.png";
-fetch("http://openweathermap.org/img.png"
-  .then(response => response)
-  // request was successful
-  .then(data =>  iconurl)
-}
-*/
+
+
