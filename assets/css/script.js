09c0182431466f5d91207d16e1d43f6d1fba1812
pat paggi <patpaggi@gmail.com>
2020-10-26 17:34:03 -0700

    day1

--- assets/css/script.js
+++ assets/css/script.js
@@ -4,65 +4,63 @@ var city = document.querySelector('.city');
 var searchBtn = document.querySelector('.button');
 var card5 = document.querySelector('.card-5')
 var place = document.querySelector('.location');
-var temp = document.querySelector('.temp')
-var wind = document.querySelector('.wind')
+var temp = document.querySelector('.temp');
+var wind = document.querySelector('.wind');
 var humidity = document.querySelector('.humidity')
 var uv = document.querySelector('uv')
-var iconcode = '';
+var iconcode = [];
+var icon = []
 var day = [];
 var days = [];
+var process = []
 
 
+var days = [];
 searchBtn.addEventListener('click', function () {
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city.value + '&units=imperial&appid=074f5d708bcfe1160c8fa4c5b0d9f5f7')
-    .then(function (response) {
+  Promise.all([
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + city.value + '&units=imperial&appid=ab02afd371ef6319765f7162754109b5'),
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city.value + '&units=imperial&appid=074f5d708bcfe1160c8fa4c5b0d9f5f7')
+  ]).then(function (responses) {
+    return Promise.all(responses.map(function (response) {
       return response.json();
-    }).then(function (data) {
-
-      // Log the data to the console
-      console.log(data);
-      day = data;
-      return fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + city.value + '&units=imperial&appid=ab02afd371ef6319765f7162754109b5');
-
-    }).then(function (response) {
-      return response.json();
-    }).then(function (data) {
-
-      console.log(data);
-      days = data;
+    }));
+  }).then(function (data) {
+    days = data
+    console.log(data);
+    showData()
+  }).catch(function (error) {
+    // if there's an error, log it
+    console.log(error);
+  });
+})
+var geticon = function () {
+  fetch("http://openweathermap.org/w/" + iconcode + "img.png")
+    .then(response => response)
+    // request was successful
+    .then(function (data) {
+      icon = data
 
     }).catch(function (error) {
       // if there's an error, log it
       console.log(error);
-    });
-})
-
-Promise.all([day, days])
-  .then(files => {
-    files.forEach(file => {
-      Process(file.json())
     })
-  })
-  .catch(err => {
+}
+var showData = function () {
 
-  })
+  iconcode = days[1].weather[0].icon;
+  place.innerHTML = days[0].city.name;
+  temp.innerText = days[1].main.temp;
+  humidity.innerText = days[1].main.humidity;
+  wind.innerText = days[1].wind.speed;
 
-var process = (prom) => {
-  prom.then(data => {
-
-  })
+  // need for loop
 }
 // five day forcast
 /*
 searchBtn.addEventListener('click', function () {
-  fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + city.value + '&units=imperial&appid=ab02afd371ef6319765f7162754109b5')
-    .then(response => response.json())
-    // request was successful
-    .then(data => {
-      var dateArr = [data["list"][6]["dt_txt"], data["list"][12]["dt_txt"], data["list"][18]["dt_txt"], data["list"][24]["dt_txt"], data["list"][30]["dt_txt"]];
-      debugger;
-      console.log(data)
-      for (let i = 0; i < dateArr.length; i++) {
+
+      var dateArr = [days["list"][6]["dt_txt"], days["list"][12]["dt_txt"], days["list"][18]["dt_txt"], days["list"][24]["dt_txt"], days["list"][30]["dt_txt"]];
+              for (let i = 0; i < dateArr.length; i++) {
         var card = document.createElement('div').className = 'col-2';
         var dateEl = document.createElement('p')
         var tempEl = document.createElement('p')
@@ -71,17 +69,13 @@ searchBtn.addEventListener('click', function () {
         card5.appendChild(card);
         card.appendChild(dateEl)
         dateEl.innerHTML = dateArr[i];
-      }
-    })
-    .catch(error => alert("opps"))
-  debugger;
-})
+
 */
 /*
 
 var  = "http://openweathermap.org/img/w/" + iconcode + "@.png";
 fetch("http://openweathermap.org/img.png"
-  .then(response => response.json())
+  .then(response => response)
   // request was successful
   .then(data =>  iconurl)
 }
